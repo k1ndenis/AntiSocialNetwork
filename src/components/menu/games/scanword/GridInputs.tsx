a33dcb95fa7496c6d50f7d039de0b34c1d739d7e
k1ndenis <d3n0s4vr@gmail.com>
2026-02-16 20:40:17 +0400

    fix(games): improve grid navigation and backspace logic

--- src/components/menu/games/scanword/GridInputs.tsx
+++ src/components/menu/games/scanword/GridInputs.tsx
@@ -11,6 +11,10 @@ export const GridInputs = (props) => {
   const [currentCells, setCurrentCells] = useState(props.questions[0].coordinates);
   const inputRefs = useRef([]);
 
+  if (!true) {
+    return <>всё</>
+  }
+
   useEffect(() => {
     if (inputRefs.current[activedCell]) {
       inputRefs.current[activedCell].focus();
@@ -44,30 +48,71 @@ export const GridInputs = (props) => {
 
   const handleChange = (e, i) => {
     const { name, value } = e.target;
+    const endPos = props.questions[currentQuestion].endPosition
+    const step = direction === "horizontal" ? 1 : 10;
+
+    if (solvedCells.includes(name)) {
+      let nextIndex = i;
+
+      while (nextIndex < endPos) {
+        nextIndex += step;
+        const nextId = `${Math.floor(nextIndex / 10)}-${nextIndex % 10}`;
+
+        if (!solvedCells.includes(nextId)) {
+          setActivedCell(nextIndex);
+          break;
+        }
+      }
+      return;
+    }
+
     const char = value.slice(-1).toUpperCase();
+    if (!char) return;
 
     const updatedCells = { ...cells, [name]: char };
     setCells(updatedCells);
 
     const isSolved = chekWord(currentQuestion, updatedCells);
-
     if (isSolved) return;
 
-    if (char) {
-      if (direction === "horizontal") {
-        setActivedCell(i + 1);
-      } else {
-        setActivedCell(i + 10);
+    let nextIndex = i;
+
+    while (nextIndex < endPos) {
+      nextIndex += step;
+      const nextId = `${Math.floor(nextIndex / 10)}-${nextIndex % 10}`;
+
+      if (!solvedCells.includes(nextId)) {
+        setActivedCell(nextIndex);
+        break;
       }
     }
-  };
+  }
 
   const handleKeyDown = (e, i) => {
     if (e.key === 'Backspace') {
-      if (direction === "horizontal" && i % 10 > 0) {
-        setActivedCell(i - 1);
-      } else if (direction === "vertical" && i >= 10) {
-        setActivedCell(i - 10);
+      const startPos = props.questions[currentQuestion].startPosition
+      const name = e.target.name
+      if (i > startPos) {
+        if (!solvedCells.includes(name)) {
+          setCells(prev => ({ ...prev, [name]: "" }));
+        }
+
+        let prevIndex = i;
+        const step = direction === "horizontal" ? 1 : 10;
+
+        while (prevIndex > startPos) {
+          prevIndex -= step;
+          const prevId = `${Math.floor(prevIndex / 10)}-${prevIndex % 10}`;
+
+          if (!solvedCells.includes(prevId)) {
+            setActivedCell(prevIndex);
+            break;
+          }
+          if (prevIndex <= startPos) {
+            setActivedCell(startPos);
+            break;
+          }
+        }
       }
     }
   }
