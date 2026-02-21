83b369e22f634c4768362b8fa6e1b4665547d810
k1ndenis <d3n0s4vr@gmail.com>
2026-02-21 20:20:37 +0400

    feat(2048): implement basic tile movement logic

--- src/components/menu/games/2048/The2048.tsx
+++ src/components/menu/games/2048/The2048.tsx
@@ -4,10 +4,10 @@ import './The2048.css'
 
 export const The2048 = () => {
   const [grid, setGrid] = useState([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [0, 2, 0, 0],
+    [0, 0, 2, 2],
+    [2, 2, 0, 0],
+    [0, 0, 0, 2],
   ]);
 
   useEffect(() => {
@@ -16,29 +16,77 @@ export const The2048 = () => {
         e.preventDefault();
       }
 
-      switch (e.key) {
-        case 'ArrowUp': {
-          console.log("'ArrowUp'");
-          break;
-        };
-        case 'ArrowDown': {
-          console.log("'ArrowDown'");
-          break;
-        };
-        case 'ArrowLeft': {
-          console.log("'ArrowLeft'");
-          break;
-        };
-        case 'ArrowRight': {
-          console.log("'ArrowRight'");
-          break;
-        };
-      }
+      setGrid(prevGrid => {
+        const newGrid = JSON.parse(JSON.stringify(prevGrid));
+        switch (e.key) {
+          case 'ArrowUp': {
+            for (let col = 0; col < 4; col++) {
+              const column = [];
+              for (let row = 0; row < 4; row++) {
+                if (newGrid[row][col] !== 0) column.push(newGrid[row][col]);
+              };
+
+              while (column.length < 4) column.push(0);
+
+              for (let row = 0; row < 4; row++) {
+                newGrid[row][col] = column[row];
+              };
+            }
+            break;
+          };
+          case 'ArrowDown': {
+            for (let col = 0; col < 4; col++) {
+              const column = [];
+              for (let row = 3; row >= 0; row--) {
+                if (newGrid[row][col] !== 0) column.push(newGrid[row][col]);
+              };
+
+              while (column.length < 4) column.push(0);
+
+              for (let row = 3, k = 0; row >= 0; row--, k++) {
+                newGrid[row][col] = column[k];
+              };
+            }
+            break;
+          };
+          case 'ArrowLeft': {
+            for (let row = 0; row < 4; row ++) {
+              const currentRow = [];
+              for (let col = 0; col < 4; col++) {
+                if (newGrid[row][col] !== 0) currentRow.push(newGrid[row][col]);
+              };
+
+              while (currentRow.length < 4) currentRow.push(0);
+
+              for (let col = 0; col < 4; col++) {
+                newGrid[row][col] = currentRow[col];
+              };
+            }
+            break;
+          };
+          case 'ArrowRight': {
+            for (let row = 0; row < 4; row ++) {
+              const currentRow = [];
+              for (let col = 3; col >= 0; col--) {
+                if (newGrid[row][col] !== 0) currentRow.push(newGrid[row][col]);
+              };
+
+              while (currentRow.length < 4) currentRow.push(0);
+
+              for (let col = 3, k = 0; col >= 0; col--, k++) {
+                newGrid[row][col] = currentRow[k];
+              };
+            }
+            break;
+          };
+        }
+        return newGrid;
+      })
     }
 
     window.addEventListener('keydown', handleKeyDown)
     return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [grid])
+  })
 
   return (
     <div className="grid-container">
