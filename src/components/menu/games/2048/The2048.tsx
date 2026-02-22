a9172677c25e3320d3714cf29281133f069b0b5b
k1ndenis <d3n0s4vr@gmail.com>
2026-02-22 11:41:24 +0400

    feat(2048): add color scheme for tiles

--- src/components/menu/games/2048/The2048.tsx
+++ src/components/menu/games/2048/The2048.tsx
@@ -1,6 +1,7 @@
 import './The2048.css'
 import { use2048 } from './bll/use2048'
-import { StartGameBtn } from './startGameBtn'
+import { StartGameBtn } from './StartGameBtn'
+import { CELLS_STYLES } from './constants/gameStyles'
 
 export const The2048 = () => {
   const { grid, startGame } = use2048()
@@ -14,7 +15,10 @@ export const The2048 = () => {
         {grid.map((row, rowIndex) => (
           <div key={rowIndex} className='grid-row'>
             {row.map((value, colIndex) => (
-              <div key={`${rowIndex}-${colIndex}`} className='grid-cell'>
+              <div 
+                key={`${rowIndex}-${colIndex}`} className='grid-cell'
+                style={CELLS_STYLES[value] || { background: "#3c3a32", color: "#f9f6f2" }}  
+              >
                 {value !== 0 ? value : ""}
               </div>
             ))}
